0eaa0cad6d5157d9cc98b75350bda3cbe405c904
Th. Abplanalp <info@tasoft.ch>
2020-10-02 17:05:14 +0200

    Initial Commit

--- components.cfg.php
+++ components.cfg.php
@@ -2,42 +2,27 @@
 
 use Skyline\Component\Config\AbstractComponent;
 use Skyline\Component\Config\CSSComponent;
-use Skyline\Component\Config\JavaScriptPostLoadComponent;
 use Skyline\Component\Config\JavaScriptComponent;
 
 return [
 	// all these keys can be required in templates like @require YourComponent
-    "YourComponent" => [
+    "Skyline" => [
     	// Use the JavaScriptComponent to load the scripts in html head tag, so before any content gets loaded.
-		"js-pre" => new JavaScriptComponent(
+		"js" => new JavaScriptComponent(
 				...AbstractComponent::makeLocalFileComponentArguments(
-				"/Public/js/your-component-uri.js",
-				__DIR__ . "/dist/skyline-component-template.min.js"
+				"/Public/Skyline/skyline.min.js",
+				__DIR__ . "/dist/skyline.min.js"
 			)
 		),
-
-		// Use the JavaScriptPostLoadComponent to load the script after loading the html page (at end of body tag).
-        "js" => new JavaScriptPostLoadComponent(
-        	...AbstractComponent::makeLocalFileComponentArguments(
-				"/Public/js/your-component-uri.js",
-				__DIR__ . "/dist/skyline-component-template.min.js"
-			)
-        ),
-
         "css" => new CSSComponent(
         	// CSS components are always loaded before the body contents.
 			...AbstractComponent::makeLocalFileComponentArguments(
-				"/Public/js/your-component-uri.css",
-				__DIR__ . "/dist/skyline-component-template.min.css",
+				"/Public/Skyline/skyline.min.css",
+				__DIR__ . "/dist/skyline.min.css",
 				'sha384',
 				NULL,
 				'all'
 			)
-		),
-        AbstractComponent::COMP_REQUIREMENTS => [
-        	// Add dependencies for the component identified by their names.
-			// Same name as you used here in this file.
-            'Skyline'
-        ]
+		)
     ]
 ];
